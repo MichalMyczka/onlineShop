0698944fff35212cfc5869424535f2d75c62e636
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-05-18 16:36:17 +0200

    add constructors and getters to admin, customer and product

--- src/main/java/com/codecool/onlineshop/Admin.java
+++ src/main/java/com/codecool/onlineshop/Admin.java
@@ -1,4 +1,7 @@
 package com.codecool.onlineshop;
 
 public class Admin extends User{
+    public Admin(int id, String email, String password, String name, String surname) {
+        super(id, email, password, name, surname);
+    }
 }
--- src/main/java/com/codecool/onlineshop/Product.java
+++ src/main/java/com/codecool/onlineshop/Product.java
@@ -1,4 +1,51 @@
 package com.codecool.onlineshop;
 
 public class Product {
+
+    private final int id;
+    private final String name;
+    private final float price;
+    private final  int quantity;
+    private final int categoryID;
+    private final Boolean isAvailable;
+    // private final int rating;
+
+    public Product(int id, String name, float price, int quantity, int categoryID,
+                   boolean isAvailable){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.categoryID = categoryID;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+//    public int getRating() {
+//        return rating;
+//    }
+
 }
